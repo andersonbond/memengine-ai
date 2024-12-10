ea9fbc19ef838821638374f09cb54be0b10e0603
KevinJustin1104 <108638169+KevinJustin1104@users.noreply.github.com>
2024-12-10 09:59:41 +0800

    add homepage

---
+++ frontend/app/pages/homepage/page.tsx
@@ -0,0 +1,33 @@
+"use client"
+import { PhoneIcon } from '@heroicons/react/24/solid';
+import { useRouter } from 'next/navigation';
+export default function Home() {
+    const router = useRouter();
+ const handleClick = () => {
+    router.push('/pages/callpage')
+ }
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-b from-blue-50 to-blue-100 text-gray-800">
+      <div className="text-center mb-8">
+        <h1 className="text-5xl font-extrabold tracking-tight mb-4 text-gray-900">
+          Welcome to <span className="text-[#f8c300]">Sgvey</span>
+        </h1>
+      </div>
+
+      <div className="flex items-center space-x-4 mt-8">
+        <button 
+            className="flex items-center px-6 py-3 bg-[#f8c300] hover:bg-[#f8c915] text-black rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition"
+            onClick={handleClick}
+        >
+            <PhoneIcon className="h-6 w-6 mr-2" />
+            Get Started
+        </button>
+      </div>
+
+
+      <footer className="absolute bottom-4 text-sm text-gray-500">
+        © {new Date().getFullYear()} Sgvey. All rights reserved.
+      </footer>
+    </div>
+  );
+}
